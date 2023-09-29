2411cb6bad587d45792be2c35001689d87bdbf8c
Sai Shivani Balamurugan <saishivanibalamurugan.sb@gmail.com>
2023-09-29 19:56:02 +0530

    Initial Commit

---
+++ github/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Sai Shivani Balamurugan on 29/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+git CLAuthorizationStatus
---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Sai Shivani Balamurugan on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
